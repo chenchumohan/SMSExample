9bc727a4e39f974e6ce189fb63611993f28e569f
chenchumohan <62737622+chenchumohan@users.noreply.github.com>
2021-08-16 17:58:47 +0530

    Update SmsPojo.java

--- src/main/java/com/example/bean/SmsPojo.java
+++ src/main/java/com/example/bean/SmsPojo.java
@@ -1,20 +1,58 @@
 package com.example.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+@Entity
+@Table(name = "smsexample")
 public class SmsPojo {
-	private String toNumber;
-    private String textMessage;
-	public String getToNumber() {
-		return toNumber;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_smsexample")
+	@SequenceGenerator(name = "seq_smsexample", sequenceName = "seq_smsexample", allocationSize = 1)
+	int SNO;
+	String customer_details;
+	 String phone_number;
+	String comments;
+	String order_details;
+	String customer_requirments;
+	public int getSNO() {
+		return SNO;
 	}
-	public void setToNumber(String toNumber) {
-		this.toNumber = toNumber;
+	public void setSNO(int sNO) {
+		SNO = sNO;
 	}
-	public String getTextMessage() {
-		return textMessage;
+	public String getCustomer_details() {
+		return customer_details;
 	}
-	public void setTextMessage(String textMessage) {
-		this.textMessage = textMessage;
+	public void setCustomer_details(String customer_details) {
+		this.customer_details = customer_details;
 	}
-    
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public String getOrder_details() {
+		return order_details;
+	}
+	public void setOrder_details(String order_details) {
+		this.order_details = order_details;
+	}
+	public String getCustomer_requirments() {
+		return customer_requirments;
+	}
+	public void setCustomer_requirments(String customer_requirments) {
+		this.customer_requirments = customer_requirments;
+	}
 	
 }
