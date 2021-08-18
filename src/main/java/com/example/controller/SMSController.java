7552577c3a531d5459cc7a346e1731b263db4975
chenchumohan <62737622+chenchumohan@users.noreply.github.com>
2021-08-18 08:46:29 +0530

    Update SMSController.java

--- src/main/java/com/example/controller/SMSController.java
+++ src/main/java/com/example/controller/SMSController.java
@@ -3,18 +3,22 @@ package com.example.controller;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bean.SmsPojo;
+import com.example.config.MailService;
 import com.example.config.SmsService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -27,11 +31,12 @@ public class SMSController {
 
     @Autowired
     private SimpMessagingTemplate webSocket;
-
+    @Autowired
+    private MailService mailService;
     private final String  TOPIC_DESTINATION = "/lesson/sms";
 
     @RequestMapping(path = { "/sms" }, method = RequestMethod.POST,consumes="application/json", produces = "application/json")
-    public SmsPojo smsSubmit(@RequestBody SmsPojo sms) {
+    public SmsPojo smsSubmit(@RequestBody SmsPojo sms) throws Exception {
         try{
         	System.out.println("sms======>"+sms.getPhone_number());
         	System.out.println("sms==message====>"+sms.getComments());
@@ -39,6 +44,7 @@ public class SMSController {
         	System.out.println("sms==Customer Requirements====>"+sms.getCustomer_requirments());
         	System.out.println("sms==Order Details====>"+sms.getOrder_details());
             service.send(sms);
+            mailService.sendEmail(sms);
         }
         catch(Exception e){
 
@@ -55,4 +61,10 @@ public class SMSController {
     private String getTimeStamp() {
        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
     }
+	/*
+	 * @GetMapping("/sendEmail") public Object sendEMail() throws MessagingException
+	 * { System.out.println("========sendEmail==="); return mailService.sendEmail();
+	 * 
+	 * }
+	 */
 }
